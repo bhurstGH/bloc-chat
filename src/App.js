7bd7e08ef80c632c3668ef55bd5017ca0e7ddf5d
Brett Hurst <bretthurst@outlook.com>
2018-10-22 12:40:22 -0700

    RoomList component created and firebase installed.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+var config = {
+  apiKey: "AIzaSyBA3YCQPOtaRz_lsapQWredZ7yLKMWzV3w",
+  authDomain: "bloc-chat-6ca07.firebaseapp.com",
+  databaseURL: "https://bloc-chat-6ca07.firebaseio.com",
+  projectId: "bloc-chat-6ca07",
+  storageBucket: "bloc-chat-6ca07.appspot.com",
+  messagingSenderId: "683680884029"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <RoomList firebase={firebase} />
       </div>
     );
   }
