f4b84f3656e19f13324373b1f8bcf2e26197ab24
Brett Hurst <bretthurst@outlook.com>
2018-10-23 01:55:40 -0700

    Added room creation functionality.

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import CreateRoom from './CreateRoom';
 
 class RoomList extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      rooms: []
+      rooms: [],
+      showCreate: false,
+      newRoom: '',
     };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
@@ -18,10 +21,43 @@ class RoomList extends Component {
     });
   }
 
+  createRoom(e, newRoom) {
+    if (newRoom) {
+      e.preventDefault();
+      this.roomsRef.push({
+        name: newRoom
+      });
+      this.cancelRoom();
+      this.setState({ newRoom: '' });
+    } else {
+    this.setState({ showCreate: true });
+    }
+  }
+
+  cancelRoom() {
+    this.setState({ showCreate: false });
+  }
+
+  handleRoomInput(e) {
+    this.setState({ newRoom: e.target.value });
+  }
+
   render() {
     return (
       <div className="room-list">
         <h1>Bloc Chat</h1>
+        <CreateRoom
+          show={this.state.showCreate}
+          cancelRoom={() => this.cancelRoom()}>
+          <form onSubmit={(e) => this.createRoom(e, this.state.newRoom)}>
+            <label>
+              New room:
+              <input type="text" value={this.state.newRoom} onChange={e => this.handleRoomInput(e)}/>
+            </label>
+              <input type="submit" value="Create room"/>
+          </form>
+        </CreateRoom>
+        <button className="create-room-button" type="button" onClick={() => this.createRoom()}>Create a room</button>
         <ul>
           {this.state.rooms.map( room =>
             <li className='room-row' key={room.key}>
